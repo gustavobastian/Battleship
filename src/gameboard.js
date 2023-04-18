c47e0621ae7f2f8301b44a0369f6bc79b6459d20
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-04-18 10:05:37 -0300

    putting some ships in the gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,8 +3,10 @@ const ship = require("./ship")
 //usefull definitions
 const OCCUPIED="O";
 const FREE="F";
+const MARK="X";
+const WATER="-";
 const VERTICAL="V";
-const HORIZONTAL="F";
+const HORIZONTAL="H";
 
 /**
  * 
@@ -21,41 +23,90 @@ const gameboard = function (xLenght,yLenght) {
     if(xlen>16){xlen=16;}
     if(ylen>16){ylen=16;}
 
-    let gameBoard = Array(ylen).fill(Array(xlen).fill(FREE));
+    //generating board
+    //let gameBoard = Array(ylen).fill(Array(xlen).fill(FREE));
+    let gameBoard=[];    
+    let line = [];
+    for(let d=0;d<xLenght;d++){
+        gameBoard[d]=[];
+        for(let i=0;i<yLenght;i++){
+            gameBoard[d][i]=FREE;
+        }
+        
+    }
+    
     let gameShips=[];
 
     let data=0;
 
-    function placeShip(xShip,yShip,size,orientation=VERTICAL){
-
-        let localShip= new ship(size);
-        
-
-        
+    function checkSpace(xShip,yShip,size,orientation=VERTICAL){
         if(orientation==HORIZONTAL){
-            if(size+xShip>xLenght){
+            if(size+xShip>xLenght){ //check if it is a valid position
                 return false;
-            }else{
-                for (let index=0;index<size;index++)
-                {
-                    gameBoard[xShip+index][yShip]=OCCUPIED;
-                    this.gameShips.push(localShip);
-                    return true;
-                }
             }
-        }
-        if(orientation==VERTICAL){
+            for (let index=0;index<size;index++){                
+                if(gameBoard[xShip+index][yShip]==OCCUPIED){
+                    return false;
+                    }                                            
+            }
+            return true;    
+        }else{
             if(size+yShip>yLenght){
                 return false;
-            }else{
-                for (let index=0;index<size;index++)
-                {
-                    gameBoard[xShip][yShip+index]=OCCUPIED;
-                    this.gameShips.push(localShip);
-                    return true;
-                }
             }
+            for (let index=0;index<size;index++){
+                if(gameBoard[xShip][yShip+index]==OCCUPIED){
+                    return false;
+                }
+                
+            }    
+            return true;
         }
+    }
+
+    function printBoard(){
+        let output="";
+        
+        for ( let i=0;i<ylen;i++){
+            output+="|";
+            let line=gameBoard[i]            
+            for ( let j=0;j<xlen;j++){                
+                output+= line[j]+"|";
+            }
+            output+="\n";
+        }
+        
+        console.log(output)
+        
+    }
+
+    function placeShip(xShip,yShip,size,orientation=VERTICAL){        
+        let localShip= new ship(size);
+        let long=parseInt(size);        
+        if(checkSpace(xShip,yShip,size,orientation)===false){
+            return false;
+        }
+        //Horizontal (placement from left to right)
+        
+        if(orientation==HORIZONTAL){                        
+            for (let index=0;index<long;index++)
+            {
+                gameBoard[xShip+index][yShip]=OCCUPIED;                  
+            }
+            this.gameShips.push(localShip);
+            return true;
+        }
+        //Vertical (placement from bottom to top)
+        if(orientation==VERTICAL){             
+            let localLine=gameBoard[xShip]                          
+            for (let index=0;index<long;index++)
+            {   
+                localLine[yShip+index]=OCCUPIED;                                
+            }          
+            this.gameShips.push(localShip);
+            return true;    
+        }
+        return true;
     }
 
     return{
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -6,7 +6,7 @@ test('testing gameboard class', () => {
     
 });
 
-test('placing a ship in 0,0 size 1', () => {
+test('placing a ship in 0,0 size 1',  () => {
   let mygameboad= new gameboard(3,4)      
   expect(mygameboad.placeShip(0,0,1)).toBe(true);
   expect(mygameboad.gameShips.length).toBe(1);
@@ -14,9 +14,32 @@ test('placing a ship in 0,0 size 1', () => {
 });
 
 
-test('placing a ship in 0,0 size 1', () => {
+test('placing a ship in 3,4 size 1',  () => {
   let mygameboad= new gameboard(3,4)      
   expect(mygameboad.placeShip(3,4,1)).toBe(false);
   expect(mygameboad.gameShips.length).toBe(0);
   expect(mygameboad.gameBoard[0][0]).toBe("F");  
+});
+
+test('placing 2 ships in 0,0 size 1',() => {
+  let mygameboad= new gameboard(4,4)      
+  expect(mygameboad.placeShip(0,0,1)).toBe(true);
+  expect(mygameboad.gameShips.length).toBe(1);
+  expect(mygameboad.gameBoard[0][0]).toBe("O");  
+  expect(mygameboad.placeShip(0,0,1)).toBe(false);
+  expect(mygameboad.gameShips.length).toBe(1);
+  expect(mygameboad.gameBoard[0][0]).toBe("O");  
+});
+
+test('placing 1 ships vertical in 0,0 size 3', () => {
+  let mygameboad= new gameboard(4,4)      
+  expect(mygameboad.placeShip(0,0,3,"V")).toBe(true);
+  expect(mygameboad.gameShips.length).toBe(1);
+  expect(mygameboad.gameBoard[0][0]).toBe("O");
+  expect(mygameboad.gameBoard[0][1]).toBe("O");
+  expect(mygameboad.gameBoard[0][2]).toBe("O");  
+  expect(mygameboad.gameBoard[1][0]).toBe("F");  
+  expect(mygameboad.gameBoard[2][0]).toBe("F");  
+  
+  
 });
