4b9333680a4669db961d238115a89df962ee787e
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-04-18 10:08:51 -0300

    other test

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -39,7 +39,27 @@ test('placing 1 ships vertical in 0,0 size 3', () => {
   expect(mygameboad.gameBoard[0][1]).toBe("O");
   expect(mygameboad.gameBoard[0][2]).toBe("O");  
   expect(mygameboad.gameBoard[1][0]).toBe("F");  
-  expect(mygameboad.gameBoard[2][0]).toBe("F");  
-  
-  
+  expect(mygameboad.gameBoard[2][0]).toBe("F");    
+});
+
+test('placing 1 ships horizontal in 0,0 size 3', () => {
+  let mygameboad= new gameboard(4,4)      
+  expect(mygameboad.placeShip(0,0,3,"H")).toBe(true);
+  expect(mygameboad.gameShips.length).toBe(1);
+  expect(mygameboad.gameBoard[0][0]).toBe("O");
+  expect(mygameboad.gameBoard[1][0]).toBe("O");
+  expect(mygameboad.gameBoard[2][0]).toBe("O");  
+  expect(mygameboad.gameBoard[0][1]).toBe("F");  
+  expect(mygameboad.gameBoard[0][2]).toBe("F");    
+});
+
+test('placing 1 ships horizontal in 1,0 size 3', () => {
+  let mygameboad= new gameboard(4,4)      
+  expect(mygameboad.placeShip(1,0,3,"H")).toBe(true);
+  expect(mygameboad.gameShips.length).toBe(1);
+  expect(mygameboad.gameBoard[0][0]).toBe("F");
+  expect(mygameboad.gameBoard[1][0]).toBe("O");
+  expect(mygameboad.gameBoard[2][0]).toBe("O");  
+  expect(mygameboad.gameBoard[3][0]).toBe("O");  
+  expect(mygameboad.gameBoard[0][2]).toBe("F");    
 });
