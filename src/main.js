6305ad9c1779b52c744795ecf7a2a0e85603f4b2
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-04-21 01:03:18 -0300

    adding main loop

--- src/main.js
+++ src/main.js
@@ -1,18 +1,61 @@
 import './style.css'; 
 let ui =require("./uI.js")
 let uiGrid =require("./ui-grid.js")
+let player=require("./player.js")
 
+
+let winner=0;
+//adding main component
 let component=ui();
 document.body.appendChild(component);
-function init(){
-let element= uiGrid.uiGrid();
-element.init();
-element.generateUI();
-element.addListener();
-}
-init();
+//
 
+let playerComputer=player("computer",10,10);
+playerComputer.fillingBoard();
+let playerHuman=player("playerName",10,10);
+playerHuman.fillingBoard();
+
+
+//initializing ui
+function init(){
+
+    let element= uiGrid.uiGrid(playerComputer,playerHuman);    
+    element.generateUI();
+    element.addListener();
+}
+
+init();
+//listener for resetting
 let general=document.getElementById('resetGame')
 general.addEventListener("click",function(){
     init();
 })
+let delayInMilliseconds = 1000; //1 second
+
+function checkGame(){
+    let localwinner=0;
+    if(playerHuman.playerBoard.checkShips()==true){
+        localwinner=1;
+        }
+    if(playerComputer.playerBoard.checkShips()==true){
+        localwinner=2;
+        }    
+        
+    return localwinner;    
+}
+window.requestAnimationFrame(gameLoop);
+
+let alert=0;
+function gameLoop() {
+    winner=checkGame();
+    if(winner!=0){
+        winner=0;
+        if(alert==0){window.alert("winner "+winner+"!");
+        alert=1;}
+        setTimeout(function(){
+            location.reload();
+        }, 1);    
+        
+    }
+    window.requestAnimationFrame(gameLoop);
+}
--- src/ui-grid.js
+++ src/ui-grid.js
@@ -1,27 +1,19 @@
 let player=require("./player.js")
 
-const uiGrid=function UiGrid() {
-    let playerComputer;
-    let playerHuman;
+const uiGrid=function UiGrid(playerComputer,playerHuman) {
     
-    function init(){
-        playerComputer=player("computer",10,10);
-        playerComputer.fillingBoard();
-        playerHuman=player("playerName",10,10);
-        playerHuman.fillingBoard();
-    }
-    function generateUI(){
-        this.init();
+    function generateUI(){        
         let gridElement = document.getElementById("player1-zone");
         gridElement.innerHTML="";
         gridElement.className = "gameGrid";
         for (let i=0;i<10;i++){
             for (let j=0;j<10;j++){
-                let elementLocal=document.createElement("button");        
-                elementLocal.id="button_"+i+"_"+j;
-                elementLocal.className="innerElement";
+                let elementLocalPlayer=document.createElement("div");        
+                elementLocalPlayer.id="button_"+i+"_"+j;
+                elementLocalPlayer.className="innerElementPlayer";
+                
                /* if(playerHuman.game)*/
-                gridElement.appendChild(elementLocal);
+                gridElement.appendChild(elementLocalPlayer);
             }
         }
 
@@ -45,23 +37,22 @@ const uiGrid=function UiGrid() {
     function addListener(){
         for (let i=0;i<10;i++){
             for (let j=0;j<10;j++){
-                let elementLocal=document.getElementById("button_"+i+"_"+j);        
+                let elementLocal=document.getElementById("CompEl_"+i+"_"+j);        
                 elementLocal.addEventListener("click",function(e){
                     console.log("button "+i+"|"+j +" pressed!");
                     let elemenComp=document.getElementById("CompEl_"+i+"_"+j);        
-                   // console.log(playerHuman.playturn(playerComputer,i,j))
                     
-                    //elemenComp.innerText="X";
                     if(playerHuman.playturn(playerComputer,i,j) == "hit")
                         {elemenComp.style.cssText="background:red;";}
                     else    
                         {elemenComp.style.cssText="background:gray;";}
+
+                    console.log(playerComputer.playturn(playerHuman,0,0));    
                 })
             }
         }
     }
-    return {
-        init,
+    return {        
         generateUI,
         addListener
 
